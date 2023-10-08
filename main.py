3d7b250f5160c9f2137661c6115d60163774a419
FacundoReyesZ <rfzamboni7@gmail.com>
2023-10-08 17:31:10 -0300

    add files

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+
 import funciones
 
 
@@ -19,7 +21,17 @@ def main():
         opc_validada = funciones.validar_opc(opc)
         if opc_validada is not None:
             if opc_validada == 1:
-                funciones.crear_archivo_binario()
+                if os.path.exists("peajes-tp4.bin"):
+                    print("El archivo 'archivo.bin' ya existe. ¿Desea eliminarlo? (s/n): ")
+                    respuesta = input()
+                    if respuesta.lower() == "s":
+                        print("El archivo binario ha sido eliminado y luego creado desde 0")
+                        os.remove("peajes-tp4.bin")
+                        funciones.crear_archivo_binario()
+                    else:
+                        print("La operación ha sido cancelada.")
+                else:
+                    funciones.crear_archivo_binario()
 
 
 if __name__ == '__main__':
